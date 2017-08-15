c6bfb5dfda7e14adb7ff0e973cda20ca2e451014
Ben Nelson <bennelson@bens-mbp.attlocal.net>
2017-08-14 23:52:03 -0700

    Updated comments

--- exercises.js
+++ exercises.js
@@ -1,109 +1,109 @@
-//Do not change any of the function names
+// Do not change any of the function names
 
 function getBiggest(x, y) {
-  //x and y are integers.  Return the larger integer
-  //if they are the same return either one
+  // x and y are integers.  Return the larger integer
+  // if they are the same return either one
 }
 
 function greeting(language) {
-  //return a greeting for three different languages:
-  //language: 'German' -> 'Guten Tag!'
-  //language: 'English' -> 'Hello!'
-  //language: 'Spanish' -> 'Hola!'
-  //if language is undefined return 'Hello!'
+  // return a greeting for three different languages:
+  // language: 'German' -> 'Guten Tag!'
+  // language: 'English' -> 'Hello!'
+  // language: 'Spanish' -> 'Hola!'
+  // if language is undefined return 'Hello!'
 }
 
 function isTenOrFive(num) {
-  //return true if num is 10 or 5
-  //otherwise return false
+  // return true if num is 10 or 5
+  // otherwise return false
 }
 
 function isInRange(num) {
-  //return true if num is less than 50 and greater than 20
+  // return true if num is less than 50 and greater than 20
 }
 
 function isInteger(num) {
-  //return true if num is an integer
-  //0.8 -> false
-  //1 -> true
-  //-10 -> true
-  //otherwise return false
-  //hint: you can solve this using Math.floor
+  // return true if num is an integer
+  // 0.8 -> false
+  // 1 -> true
+  // -10 -> true
+  // otherwise return false
+  // hint: you can solve this using Math.floor
 }
 
 function fizzBuzz(num) {
-  //if num is divisible by 3 return 'fizz'
-  //if num is divisible by 5 return 'buzz'
-  //if num is divisible by 3 & 5 return 'fizzbuzz'
-  //otherwise return num
+  // if num is divisible by 3 return 'fizz'
+  // if num is divisible by 5 return 'buzz'
+  // if num is divisible by 3 & 5 return 'fizzbuzz'
+  // otherwise return num
 }
 
 function isPrime(num) {
-  //return true if num is prime.
-  //otherwise return false
-  //hint: a prime number is only evenly divisible by itself and 1
-  //hint2: you can solve this using a for loop
-  //note: 0 and 1 are NOT considered prime numbers
+  // return true if num is prime.
+  // otherwise return false
+  // hint: a prime number is only evenly divisible by itself and 1
+  // hint2: you can solve this using a for loop
+  // note: 0 and 1 are NOT considered prime numbers
 }
 
 function returnFirst(arr) {
-  //return the first item from the array
+  // return the first item from the array
 }
 
 function returnLast(arr) {
-  //return the last item of the array
+  // return the last item of the array
 }
 
 function getArrayLength(arr) {
-  //return the length of the array
+  // return the length of the array
 }
 
 function incrementByOne(arr) {
-  //arr is an array of integers  
-  //increase each integer by one
-  //return the array
+  // arr is an array of integers  
+  // increase each integer by one
+  // return the array
 }
 
 function addItemToArray(arr, item) {
-  //add the item to the end of the array
-  //return the array
+  // add the item to the end of the array
+  // return the array
 }
 
 function addItemToFront(arr, item) {
-  //add the item to the front of the array
-  //return the array
-  //hint: use the array method .unshift
+  // add the item to the front of the array
+  // return the array
+  // hint: use the array method .unshift
 }
 
 function wordsToSentence(words) {
-  //words is an array of strings
-  //return a string that is all of the words concatenated together
-  //spaces need to be between each word
-  //example: ['Hello', 'world!'] -> 'Hello world!'
+  // words is an array of strings
+  // return a string that is all of the words concatenated together
+  // spaces need to be between each word
+  // example: ['Hello', 'world!'] -> 'Hello world!'
 }
 
 function contains(arr, item) {
-  //check to see if item is inside of arr
-  //return true if it is, otherwise return false
+  // check to see if item is inside of arr
+  // return true if it is, otherwise return false
 }
 
 function addNumbers(numbers) {
-  //numbers is an array of integers.
-  //add all of the integers and return the value
+  // numbers is an array of integers.
+  // add all of the integers and return the value
 }
 
 function averageTestScore(testScores) {
-  //testScores is an array.  Iterate over testScores and compute the average.
-  //return the average
+  // testScores is an array.  Iterate over testScores and compute the average.
+  // return the average
 }
 
 function largestNumber(numbers) {
-  //numbers is an array of integers
-  //return the largest integer
+  // numbers is an array of integers
+  // return the largest integer
 }
 
-//Do not modify code below this line.
-////--------------------------------
+// Do not modify code below this line.
+// --------------------------------
 
 module.exports = {
   getBiggest: getBiggest,
